c7ad784a1f931328f35bd404067ecf213f1ff557
Michael Noseworthy <mnoseworthy7@gmail.com>
2018-01-13 00:54:49 -0500

    Verify shuffle appears random

--- src/utils.test.js
+++ src/utils.test.js
@@ -12,4 +12,24 @@ it('counts the number of resource cards', () => {
   let cards = [s, s, r, r, s];
 
   expect(countResourceCards(cards)).toBe(2);
-})
+});
+
+it('show shuffle distribution', () => {
+  let l = [1,2,3,4];
+  let counts = {};
+  
+  for (let n = 0; n < 1000; n++) {
+    let tmp = l.slice();
+    tmp = shuffle(tmp);
+    let str = "";
+    for (let i = 0; i < tmp.length; i++) {
+      str = str + tmp[i].toString();
+    }
+    if (str in counts) {
+      counts[str] += 1;
+    } else {
+      counts[str] = 0;
+    }
+  }
+  //console.log(counts);
+});
