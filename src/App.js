8c655906c8ce03620a8b3df75098c153aeeed3fd
Michael Noseworthy <mnoseworthy7@gmail.com>
2018-01-27 16:21:14 -0500

    Show whose turn it is by bolding their score

--- src/App.js
+++ src/App.js
@@ -54,6 +54,18 @@ class JaipurBoard extends Component {
       tokenCells.push(<TokenDisplay tokenType={key} tokenValues={this.props.G.bonusTokens[key]} hidden={true} />)
     }
 
+    let youStyle = {}
+    let oppStyle = {}
+    if (this.props.playerID === this.props.ctx.currentPlayer) {
+      youStyle.fontWeight = 'bold';
+      oppStyle.fontWeight = 'normal';
+    } else {
+      console.log(this.props.playerID);
+      console.log(this.props.ctx.currentPlayer);
+      oppStyle.fontWeight = 'bold';
+      youStyle.fontWeight = 'normal';
+    }
+
     return (
     <div class="boardDiv">
       <div class="tokenDiv">
@@ -79,11 +91,11 @@ class JaipurBoard extends Component {
       </div>
 
       <div class="scoreDiv">
-        <p> You: {this.props.G.players[this.props.playerID].tokens.reduce((a, b) => a+b, 0)} </p>
+        <p style={youStyle}> You: {this.props.G.players[this.props.playerID].tokens.reduce((a, b) => a+b, 0)} </p>
         <div class="card">
           {this.props.G.deck.length}
         </div>
-        <p> Opponent: {this.props.G.players[1-this.props.playerID].tokens.reduce((a,b) => a+b, 0)} </p>
+        <p style={oppStyle}> Opponent: {this.props.G.players[1-this.props.playerID].tokens.reduce((a,b) => a+b, 0)} </p>
       </div>
     </div>
     );
